551a1487923b41b7be9b3094aacc3976b4c96141
Katie Bambino <katie.bambino@gmail.com>
2014-07-20 17:02:01 -0400

    Initial travis-ci config

---
+++ .travis_setup.sh
@@ -0,0 +1,32 @@
+# Sets up Lua and Luarocks for travis-ci testing.
+# LUA_VERSION must be "Lua 5.1"; you can see WildStar's
+# executed version via `/eval Print(_VERSION)` in-game.
+
+echo 'rocks_servers = {
+  "http://rocks.moonscript.org/",
+  "http://luarocks.org/repositories/rocks"
+}' >> ~/config.lua
+
+
+if [ "$LUA" == "Lua 5.1" ]; then
+  curl http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
+  cd lua-5.1.5;
+  sudo make linux install;
+fi
+
+cd ..
+curl http://luarocks.org/releases/luarocks-2.1.2.tar.gz | tar xz
+cd luarocks-2.1.2
+./configure;
+
+make && sudo make install
+cd ..
+
+git clone https://github.com/LuaDist/lua.git --quiet
+cd lua/
+git checkout -qf $LUA_VERSION
+cmake .
+make
+sudo make install
+cd ../
+make
